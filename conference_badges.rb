c04e1fc77e80f40e908419d638b303e4ca6b676a
Andre Santiago <the.asantiagojr@gmail.com>
2018-06-19 16:55:04 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def batch_badge_creator(arr)
+  arr
+end
+
+def assign_rooms(arr)
+  puts "Hello #{arr}! You'll be assigned to room #{room}!"
+end
