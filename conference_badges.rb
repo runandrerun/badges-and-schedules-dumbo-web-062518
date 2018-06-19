336de93c6aae43cbcbd05439a68fb6a0782be6c8
Andre Santiago <the.asantiagojr@gmail.com>
2018-06-19 16:55:56 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,6 +2,10 @@
 
 speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
 def batch_badge_creator(arr)
   arr
 end
