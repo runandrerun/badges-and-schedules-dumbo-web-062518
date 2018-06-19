4c0025c37fae258135ef4ff8c88fe6a2d801dc26
Andre Santiago <the.asantiagojr@gmail.com>
2018-06-19 17:34:14 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ end
 
 def assign_rooms(arr)
   rooms = []
-  counter = 1
+  counter = 0 
   arr.each { |x| rooms << "Hello, #{x}! You'll be assigned to room #{counter}!" }
   counter += 1
   return rooms
